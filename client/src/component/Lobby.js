cf96e5f8209150b96eb4eda1a46b05363ed7df1b
Leafurya <audrb5300@naver.com>
2023-02-07 20:26:34 +0900

    dev12
    give up close function

--- client/src/component/Lobby.js
+++ client/src/component/Lobby.js
@@ -25,24 +25,6 @@ function Lobby(props){
 				<ProjectLists projects={props.projects} PageCallback={props.PageCallback}></ProjectLists>
 			</ul>
 			<div className="navi_btns">
-				<input className="close_btn" type="button" value="CLOSE" onClick={
-					()=>{
-						closeBtnClickCount++;
-						if(closeBtnClickCount>=2){
-							window.close();
-						}
-						else{
-							closeNotiEle.classList="close_noti"
-							document.querySelector(".App").appendChild(closeNotiEle)
-							closeInterval=setInterval(()=>{
-								closeBtnClickCount=0;
-								closeNotiEle.classList.remove("close_noti")
-								document.querySelector(".App").removeChild(closeNotiEle)
-								clearInterval(closeInterval)
-							},1990)
-						}
-					}
-				}></input>
 				<input type="button" className="information_btn" value="정보" onClick={()=>{
 					alert("버전: alpha 1.0.0\n연속 출석 "+props.attendance+"일째");
 				}}></input>
