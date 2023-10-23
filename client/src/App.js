1f3a71e0eaf8ebfe35da8d82a3534791ad846f32
Leafurya <audrb5300@naver.com>
2023-10-24 08:57:11 +0900

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,39 +9,6 @@ import {InitAttendance} from './module/AttendanceModule.js'
 import projectBundle from './module/global/DataBundle';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getCount } from './module/global/Count';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAkHhMK0XzCao6sAoG8hfglLtYd9zxS40I",
-  authDomain: "dday-38612.firebaseapp.com",
-  projectId: "dday-38612",
-  storageBucket: "dday-38612.appspot.com",
-  messagingSenderId: "1077048953916",
-  appId: "1:1077048953916:web:04bc21c7e0f189a3ae0739",
-  measurementId: "G-Q7N9YZYMJP"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-const CreateNotification=async(tag, title, timestamp)=>{
-	const registration=await navigator.serviceWorker.getRegistration()
-	console.log(registration)
-	registration.showNotification(title,{
-		tag:tag,
-		body:"time stamp",
-		showTrigger:new TimestampTrigger(timestamp+30*1000)
-	})
-}
-
 function App() {
 	const [re,setRe]=useState([])
 	useEffect(()=>{
@@ -49,11 +16,6 @@ function App() {
 		InitDate()
 		projectBundle.Init()
 		setRe([])
-		Notification.requestPermission((result)=>{
-			if(result==="granted"){
-				CreateNotification("tag","title",Date.now())
-			}
-		})
 	},[])
 	
 	console.log("app",projectBundle)
@@ -79,18 +41,4 @@ function App() {
 	}
 }
 
-export function ShowNotification(){
-	Notification.requestPermission((result)=>{
-		if(result==="granted"){
-			navigator.serviceWorker.ready.then((registration)=>{
-				registration.showNotification("noti sample",{
-					body:"body",
-					icon:"./logo.svg",
-					vibrate:[100,100,200,200,300,300],
-					tag:"noti tag"
-				})
-			})
-		}
-	})
-}
 export default App;
