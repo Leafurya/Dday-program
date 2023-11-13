9d03652c16f572473da1c4ecb71efb955ee0895f
Leafurya <46297197+Leafurya@users.noreply.github.com>
2023-11-13 16:28:52 +0900

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,16 @@ import todoList from './module/global/ToDo';
 import ToDoToday from './component/ToDoToday';
 import ToDoModify from './component/ToDoModify';
 
+window.addEventListener('beforeinstallprompt',(e)=>{
+	e.preventDefault()
+	console.log("before install")
+	if(window.matchMedia('(display-mode: standalone').matches){
+		console.log("standalone")
+	}
+	else{
+		console.log("install")
+	}
+})
 function App() {
 	const install=useRef()
 	const [re,setRe]=useState([])
@@ -37,17 +47,7 @@ function App() {
 	// 		});
 	// 	}
 	// });
-	window.addEventListener('beforeinstallprompt',(e)=>{
-		e.preventDefault()
-		console.log("before install")
-		if(window.matchMedia('(display-mode: standalone').matches){
-			install.current="standalone"
-		}
-		else{
-			install.current="install"
-		}
-		setRe([])
-	})
+	
 	
 	
 	useEffect(()=>{
