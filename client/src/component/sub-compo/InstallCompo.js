389f66816f8402bf93a5785aa18897374a1eff87
Leafurya <46297197+Leafurya@users.noreply.github.com>
2023-11-14 17:54:51 +0900

    Update InstallCompo.js

--- client/src/component/sub-compo/InstallCompo.js
+++ client/src/component/sub-compo/InstallCompo.js
@@ -8,7 +8,7 @@ export default ()=>{
 		console.log(installEvent.current)
 		return false
 	})
-	const installBtnBallback=useCallback(()=>{
+	const installBtnCallback=useCallback(()=>{
 		let event=installEvent.current
 		if(event===undefined){
 			return
@@ -26,8 +26,14 @@ export default ()=>{
 	})
 	return(
 		<div>
-			install
-			<input type="button" onClick={installBtnBallback} value="설치하기"></input>
+			<div className="app_information">
+				앱 정보가 보여야된다.
+				앱 정보라 함은 일단 개발자가 누군지, 앱 이름이 뭔지, 이 앱의 목적이 뭔지 정도 있으면 되지 않을까?
+				앱 사용법.
+			</div>
+			<div className="install_btn_platform">
+				<input type="button" onClick={installBtnCallback} value="설치하기"></input>
+			</div>
 		</div>
 	)
 }
